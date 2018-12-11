9b26faa884089a9d5e253fb47224270079eb9801
Matt Dowell <mattdowell12@gmail.com>
2018-12-11 10:34:52 -0600

    set up for part 2 but we r 2 slow man

--- day11/chronal_charge.py
+++ day11/chronal_charge.py
@@ -1,9 +1,22 @@
+import time
 
 SQUARE_WIDTH = 300
-SUB_GRID_WIDTH = 3
 
 
-def find_max_grid(serial_number):
+def find_max_grid_any_sub_grid_size(serial_number):
+    # (grid_size, (top_left_x, top_left_y), power)
+    best = None
+    for sub_grid_size in range(1, SQUARE_WIDTH + 1):
+        start = time.time()
+        max_grid_coord, power = find_max_grid(serial_number, sub_grid_size)
+        if best is None or power > best[-1]:
+            best = (sub_grid_size, max_grid_coord, power)
+
+        print "Finished calculating for sub grid size " + str(sub_grid_size) + " took " + str(time.time() - start) + "s"
+
+    return best[1], best[0]
+
+def find_max_grid(serial_number, sub_grid_size):
     coord_to_power_level = {coord: cell_power_level(coord, serial_number) \
         for coord in _coord_generator()}
 
@@ -13,21 +26,22 @@ def find_max_grid(serial_number):
     highest_so_far = (None, None)
 
     coords_to_try = [c for c in _coord_generator() \
-        if c[0] <= SQUARE_WIDTH - (SUB_GRID_WIDTH - 1) and c[1] <= SQUARE_WIDTH - (SUB_GRID_WIDTH - 1)]
+        if c[0] <= SQUARE_WIDTH - (sub_grid_size - 1) \
+        and c[1] <= SQUARE_WIDTH - (sub_grid_size - 1)]
 
     for top_left in coords_to_try:
-        sub_grid_power = _total_subgrid_power(top_left, coord_to_power_level)
+        sub_grid_power = _total_subgrid_power(top_left, coord_to_power_level, sub_grid_size)
         if highest_so_far[0] is None or sub_grid_power > highest_so_far[1]:
             highest_so_far = (top_left, sub_grid_power)
 
-    return highest_so_far[0]
+    return highest_so_far
 
 
-def _total_subgrid_power(top_left, coord_to_power_level):
+def _total_subgrid_power(top_left, coord_to_power_level, sub_grid_size):
     start_x, start_y = top_left
 
-    sub_grid_coords = ((x, y) for x in range(start_x, start_x + SUB_GRID_WIDTH) \
-        for y in range(start_y, start_y + SUB_GRID_WIDTH))
+    sub_grid_coords = ((x, y) for x in range(start_x, start_x + sub_grid_size) \
+        for y in range(start_y, start_y + sub_grid_size))
 
     powers = [coord_to_power_level[coord] for coord in sub_grid_coords]
 
@@ -55,5 +69,8 @@ def cell_power_level(coordinate, serial_number):
 
 
 if __name__ == "__main__":
-    max_grid_upper_left = find_max_grid(4151)
-    print "Max grid:", max_grid_upper_left
+    max_grid_upper_left = find_max_grid(4151, 3)
+    print "Part 1 max grid:", max_grid_upper_left
+
+    part_2_best = find_max_grid_any_sub_grid_size(4151)
+    print "Part 2 max grid with sub grid size:", part_2_best
